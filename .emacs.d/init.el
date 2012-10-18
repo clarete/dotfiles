84d434ef573d924794bd090a2826ab8229696688
Lincoln de Sousa <lincoln@comum.org>
2012-10-18 19:03:30 -0400

    [emacs] Activating flyspell for all markdown files by default

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -154,6 +154,7 @@
 (autoload 'markdown-mode "markdown-mode.el"
   "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
+(add-hook 'markdown-mode-hook '(lambda() (flyspell-mode)))
 
 ;; vala mode
 (autoload 'vala-mode "vala-mode" "Major mode for editing Vala code." t)
