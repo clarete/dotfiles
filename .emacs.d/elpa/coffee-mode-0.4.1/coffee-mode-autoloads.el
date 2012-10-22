c1b09c0af6df0204a9c59a98765bf3cd9d198c7c
Lincoln de Sousa <lincoln@comum.org>
2012-10-21 23:29:46 -0400

    [emacs] Updating the coffee-mode extension

--- .emacs.d/elpa/coffee-mode-0.4.1/coffee-mode-autoloads.el
+++ .emacs.d/elpa/coffee-mode-0.4.1/coffee-mode-autoloads.el
@@ -3,9 +3,24 @@
 ;;; Code:
 
 
-;;;### (autoloads (coffee-mode) "coffee-mode" "coffee-mode.el" (20536
-;;;;;;  11946))
+;;;### (autoloads (coffee-mode) "../../../Work/Conf/dotfiles/.emacs.d/elpa/coffee-mode-0.4.1/coffee-mode"
+;;;;;;  "coffee-mode.el" "3e588d72c1e873e6331a3e1b5a1403f7")
 ;;; Generated autoloads from coffee-mode.el
+
+(autoload 'coffee-mode "../../../Work/Conf/dotfiles/.emacs.d/elpa/coffee-mode-0.4.1/coffee-mode" "\
+Major mode for editing CoffeeScript.
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
+
+(add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
+
+;;;***
+
+;;;### (autoloads (coffee-mode) "coffee-mode" "../../../../../../.emacs.d/elpa/coffee-mode-0.4.1/coffee-mode.el"
+;;;;;;  "3e588d72c1e873e6331a3e1b5a1403f7")
+;;; Generated autoloads from ../../../../../../.emacs.d/elpa/coffee-mode-0.4.1/coffee-mode.el
 
 (autoload 'coffee-mode "coffee-mode" "\
 Major mode for editing CoffeeScript.
@@ -18,7 +33,10 @@ Major mode for editing CoffeeScript.
 
 ;;;***
 
-;;;### (autoloads nil nil ("coffee-mode-pkg.el") (20536 11946 472133))
+;;;### (autoloads nil nil ("../../../../../../.emacs.d/elpa/coffee-mode-0.4.1/coffee-mode-autoloads.el"
+;;;;;;  "../../../../../../.emacs.d/elpa/coffee-mode-0.4.1/coffee-mode-pkg.el"
+;;;;;;  "../../../../../../.emacs.d/elpa/coffee-mode-0.4.1/coffee-mode.el"
+;;;;;;  "coffee-mode.el") (20604 21143 136819 0))
 
 ;;;***
 
