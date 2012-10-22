1c3a934f6492a529c994ebfbecd7c45453bb2656
Lincoln de Sousa <lincoln@comum.org>
2012-10-21 23:31:49 -0400

    [emacs] More utf-8 stuff

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,9 +8,11 @@
 
 ;; utf-8 for good
 (prefer-coding-system 'utf-8)
+(setq locale-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
 
 ;; Basic config for columns
 (column-number-mode)
