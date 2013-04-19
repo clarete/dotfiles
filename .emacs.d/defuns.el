10480f6afbb74f509e4b028b7f41bfe913058ac7
Lincoln de Sousa <lincoln@comum.org>
2013-04-19 11:47:20 -0400

    [emacs] Removing crazy attempt to fix python indentation

--- .emacs.d/defuns.el
+++ .emacs.d/defuns.el
@@ -24,23 +24,3 @@ please, be careful, once called, it can't be stopped!"
                (revert-buffer 1 1))
              (kill-buffer b))
           (buffer-list)))
-
-
-(defadvice python-calculate-indentation (around outdent-closing-brackets)
-  "Handle lines beginning with a closing bracket and indent them so that
-they line up with the line containing the corresponding opening bracket."
-  (save-excursion
-    (beginning-of-line)
-    (let ((syntax (syntax-ppss)))
-      (if (and (not (eq 'string (syntax-ppss-context syntax)))
-               (python-continuation-line-p)
-               (cadr syntax)
-               (skip-syntax-forward "-")
-               (looking-at "\\s)"))
-          (progn
-            (forward-char 1)
-            (ignore-errors (backward-sexp))
-            (setq ad-return-value (current-indentation)))
-        ad-do-it))))
-
-(ad-activate 'python-calculate-indentation)
