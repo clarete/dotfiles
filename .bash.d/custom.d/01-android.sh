76c48190362b5c5cdc4168741ab9970a0c6823d4
Lincoln de Sousa <lincoln@comum.org>
2013-04-21 20:40:04 -0400

    [bash] Adding variables to my android sdk path

---
+++ .bash.d/custom.d/01-android.sh
@@ -0,0 +1,6 @@
+# -*- Mode: shell-script; -*-
+
+# Android stuff
+export ANDROID_SDK_PATH="$HOME/Library/Local/adt-bundle-mac-x86_64-20130219/sdk"
+PATH="$PATH:$ANDROID_SDK_PATH/tools"
+PATH="$PATH:$ANDROID_SDK_PATH/platform-tools"
